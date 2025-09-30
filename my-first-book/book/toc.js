f56b99c238cd6d23a5d8cc0d8e108218fed93481
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-09-30 21:05:21 +0200

    added my-path as a new readme

--- my-first-book/book/toc.js
+++ my-first-book/book/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="chapter_1.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="1.Introduction/1.1.resources.html"><strong aria-hidden="true">1.1.</strong> The Different learning Resources</a></li></ol></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="chapter_1.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="1.Introduction/1.1.resources.html"><strong aria-hidden="true">1.1.</strong> The Different learning Resources</a></li><li class="chapter-item expanded "><a href="1.introduction/1.2.my_path.html"><strong aria-hidden="true">1.2.</strong> My Path</a></li></ol></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
--- my-first-book/src/SUMMARY.md
+++ my-first-book/src/SUMMARY.md
@@ -2,3 +2,4 @@
 
 - [Introduction](./chapter_1.md)
   - [The Different learning Resources](./1.Introduction/1.1.resources.md)
+  - [My Path](./1.introduction/1.2.my_path.md)
